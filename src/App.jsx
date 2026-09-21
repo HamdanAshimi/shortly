826c53dc8fe20b544a5703b51d341d4cbba8ec7b
HamdanAshimi <ashimiademola4@gmail.com>
2026-09-21 14:48:27 +0100

    Add advanced statistics section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import Shorten from "./components/Shorten";
+import Stats from "./components/Stats";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Nav />
       <Hero />
       <Shorten />
+      <Stats/>
     </>
   );
 }
